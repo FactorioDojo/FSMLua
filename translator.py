8659373e1a9a0d0e819ae4d3fe1c97daad9b9716
Henry Baldwin <henrymbaldwin@gmail.com>
2023-06-22 17:09:08 -0400

    non functional

--- translator.py
+++ translator.py
@@ -1,27 +1,42 @@
 import os
 os.environ["PATH"] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
 
-import luaparser.ast as ast
+from lua_parser import parse
 from graphviz import Digraph
 
-class FSMTranslator(ast.ASTVisitor):
+class FSMTranslator:
     def __init__(self):
         self.graph = Digraph()
+        self.current_function = None
     
     def translate(self, tree):
         self.visit(tree)
         return self.graph
     
-    def visit_FunctionDef(self, node):
-        self.visit(node.body)
+    def visit(self, node):
+        method_name = f"visit_{type(node).__name__}"
+        visitor = getattr(self, method_name, self.generic_visit)
+        return visitor(node)
+    
+    def generic_visit(self, node):
+        for child in node.children():
+            self.visit(child)
+    
+    def visit_Function(self, node):
+        function_name = self.visit(node.name)
+        self.graph.node(function_name)
+        if self.current_function:
+            self.graph.edge(self.current_function, function_name)
+        self.current_function = function_name
+        self.generic_visit(node)
+        self.current_function = None
     
     def visit_Call(self, node):
-        function_name = node.func.id
+        function_name = self.visit(node.func)
         self.graph.node(function_name)
-        self.visit(node.func)
-        for arg in node.args:
-            self.visit(arg)
-            self.graph.edge(function_name, str(arg))
+        if self.current_function:
+            self.graph.edge(self.current_function, function_name)
+        self.generic_visit(node)
     
     def visit_Name(self, node):
         return node.id
@@ -35,7 +50,7 @@ function doThing()
 end
 """
 
-tree = ast.parse(code)
+tree = parse(code)
 translator = FSMTranslator()
 graph = translator.translate(tree)
 
